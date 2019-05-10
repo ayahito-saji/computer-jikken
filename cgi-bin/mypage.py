9a5668b0ddb8e51ea7728898937a00c51fc5c49b
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2019-05-10 17:33:29 +0900

    ホームとマイページの完成

--- cgi-bin/mypage.py
+++ cgi-bin/mypage.py
@@ -21,6 +21,7 @@ def application(environ,start_response):
     html = '<html lang="ja">\n' \
            '<head>\n' \
            '<meta charset="UTF-8">\n' \
+           '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"><script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">' \
            '<title>mypage</title>\n' \
            '<link rel="stylesheet" href="default.css">\n' \
            '</head>\n'
@@ -44,8 +45,16 @@ def application(environ,start_response):
         start_response(status,response_headers)
         return ''
 
-    html += '<body>\n' \
-            '<h1>マイページ</h1>'
+    html += '<body style="background: #398235;ackground: -moz-linear-gradient(45deg, #398235 0%, #8ab66b 56%, #c9de96 100%);background: -webkit-linear-gradient(45deg, #398235 0%,#8ab66b 56%,#c9de96 100%);background: linear-gradient(45deg, #398235 0%,#8ab66b 56%,#c9de96 100%);">\n' \
+            '<div class="container">' \
+            '<div class="row"><div class="col s12"><h1 class="white-text">マイページ</h1></div></div>'
+
+    html += '<form method = "POST">'
+    html += '<div class="row">'
+    html += '<div class="col s6"><a href="/home" class="btn btn-large" style="width:100%"><i class="fas fa-home">ホーム</i></a></div>'
+    html += '<div class="col s6"><button type = "submit" name = "logout" class="btn-large red darken-1" style="width:100%"><i class="fas fa-sign-out-alt">ログアウト</i></button></div>'
+    html += '</div>'
+    html += '</form>'
 
     con_fab = sqlite3.connect(fabdb)
     con_fab.text_factory = str
@@ -72,7 +81,7 @@ def application(environ,start_response):
     tweet_ids.sort()
 
     #html文の作成
-    html += '<ol>\n'
+    html += '<div class="row">\n'
     for i in range(len(tweet_ids)):
         sql = 'select user_id from tweet where tweet_id = ?'
         for row in cur_tweet.execute(sql, (str(tweet_ids[i]), )):
@@ -83,26 +92,22 @@ def application(environ,start_response):
         sql = 'select body from tweet where tweet_id = ?'
         for row in cur_tweet.execute(sql, (str(tweet_ids[i]), )):
             tweet_body = row[0]
-            html += '<li>'
-            html += 'tweet<br>-----------------<br>\n' + str(tweet_body) + '<br>\n'
-	    html += 'user : ' + tweet_user_id + '\n'
-            html += 'fav: ' + str(tweet_fab_count) + '<br>'
-            html += '<br>'
-            html += '</li>\n'
+            html += '<div class="col s12 m6 l4"><div class="card"><div class="card-content">'
+            html += '<span class="card-title">' + tweet_user_id + '</span>'
+            html += '<p>' + str(tweet_body) + '</p>'
+            html += '<i class="pink-text fas fa-heart"></i></span>共感:' + str(tweet_fab_count) + '<br>'
+            html += '</div></div>'
+            html += '</div>\n'
 
-    html += '</ol>\n'
+    html += '</div>\n'
 
     con_fab.commit()
 
     cur_fab.close()
     con_fab.close()
 
-    html += '<form method = "POST">'
-    html += '<button type = "submit" name = "logout"> logout </button>'
-    html += '</form>'
-
-    html += '<a href="/home">Go to Home</a><br>'
-    html += '</body>\n'
+    html += '<a class="btn-floating btn-large waves-effect waves-light pulse blue" style="position: fixed; bottom: 32px; right: 32px;" href="/tweet"><i class="fas fa-comment"></i></a>'
+    html += '</div></body>\n'
 
     html += '</html>\n'
     #response_header = [('Content-type','text/html')]
